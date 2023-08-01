6ae77de60a8fda0b1fde2af1a4ab2b79f4525d88
Cassandra <semisentientfungus@gmail.com>
2023-08-01 16:46:56 +0200

    add module for tracking game

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-const game = (() => {
-    const gameBoard = ["", "", "", "", "", "", "", "", ""];
+const gameBoard = (() => {
+    const board = new Array(9);
 
-    const move = (index, symbol) => gameBoard[index] = symbol;
+    const move = (index,symbol) => {
+        grid.children[index].innerHTML = symbol;
+    };
 
-    return {gameBoard, move};
+    return {board, move};
+})();
+
+const displayController = (() => {
+    const log = (player) => console.log(`it is ${player.name}'s turn`);
+
+    const turns = (player1, player2) => {
+        let i = 0;
+        let win = false;
+        while (i < 9 && win == false){
+            if (i%2 == 0){
+                currentPlayer = player1;
+            }
+            else {
+                currentPlayer = player2;
+            }
+            displayController.log(currentPlayer)
+            i++;
+        }
+    };
+    return {log, turns};
 })();
 
 //factory function for player characters
 const   playerFactory = (name, symbol) => {
-    const sayHi = () => console.log('Hi! my name is ' + name);
-    return {name, symbol, sayHi}
+    //const playerMove = (index) => game.move(index, symbol);
+    const playerMove = (index) => game.move(index, symbol)
+    return {name, symbol, playerMove};
 }
 
 const Cass = playerFactory('Cassidy', 'X');
 const Alex = playerFactory('Alexandra', 'O');
 
+const grid = document.querySelector('.grid-container');
